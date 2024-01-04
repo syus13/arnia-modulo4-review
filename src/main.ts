d6c0ab9fe021ed6cac39b163abf14881370edd1a
syus13 <76135546+syus13@users.noreply.github.com>
2024-01-03 23:24:34 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   const config = new DocumentBuilder()
-    .setTitle('Arnia Revisão')
-    .setDescription('The Arnia API description')
+    .setTitle('Review')
+    .setDescription('API description')
     .setVersion('1.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(+configService.get('APP_PORT') || 3000);
 }
 bootstrap();
